8f8ffaacf7a947ca94e1debeef5e22d0de396ece
aliciasavelly <alicia.savelly@gmail.com>
2017-02-05 22:16:34 -0800

    add to poker game

--- poker/lib/deck.rb
+++ poker/lib/deck.rb
@@ -1,20 +1,21 @@
 require_relative "card.rb"
+require_relative "hand.rb"
 
 class Deck
 
-  attr_accessor :cards
+  # attr_reader :cards
 
   SUITS = {spades: 1, hearts: 2, diamonds: 3, clubs: 4}
   VALUES = {two: 2, three: 3, four: 4, five: 5, six: 6,
             seven: 7, eight: 8, nine: 9, ten: 10, jack: 11,
             queen: 12, king: 13, ace: 14}
 
-  def initialize
-    @cards = Deck.generate_deck
+  def initialize(cards = Deck.all_cards)
+    @cards = cards
     7.times { @cards.shuffle! }
   end
 
-  def self.generate_deck
+  def self.all_cards
     deck = []
     SUITS.keys.each do |suit|
       VALUES.keys.each do |value|
@@ -22,6 +23,29 @@ class Deck
       end
     end
     deck
+  end
+
+  def count
+    @cards.count
+  end
+
+  def take(n)
+    raise "not enough cards" if n > count
+    taken = []
+    n.times { taken << @cards.shift }
+    taken
+  end
+
+  def return(more_cards)
+    @cards += more_cards
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
+
+  def deal_hand
+    Hand.new(take(5))
   end
 
 end
--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -1,3 +1,17 @@
 class Hand
+  attr_accessor :cards
 
+  def initialize(cards)
+    @cards = cards
+    raise "must have five cards" if @cards.length != 5
+  end
+
+  def trade_cards(take_cards, new_cards)
+    take_cards.each do |card|
+      raise "cannot discard unowned card" unless @cards.include?(card)
+      @cards.delete(card)
+    end
+    @cards += new_cards
+    raise "must have five cards" if @cards.length != 5
+  end
 end
